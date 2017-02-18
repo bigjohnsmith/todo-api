6ee0b54287b4376535cf0b6b3ec617361187cd2d
Graham Breckney <graham_breckney@hotmail.co.uk>
2017-02-18 00:40:18 +0000

    add POST for todos

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: "Pay the dentist",
-	completed: false
-}, {
-		id: 2,
-	description: "Pick up veg at market",
-	completed: false
-}, {
-		id: 3,
-	description: "Get baby bottles and such",
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
 	res.send('Todo API Root');
@@ -39,6 +31,18 @@ app.get('/todos/:id', function (req, res) {
 	} else {
 		res.status(404).send();
 	}
+});
+
+// POST /todos
+app.post('/todos', function (req, res) {
+	var body = req.body;
+	
+	// add id field
+	body.id = todoNextId++;
+	// push body into array
+	todos.push(body);
+	
+	res.json(body);
 });
 
 app.listen(PORT, function () {
